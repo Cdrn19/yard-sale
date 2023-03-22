647a1012513ba24009121d40cd25cd1527c770fe
Carlos Revetti <cdrn_19@hotmail.com>
2023-03-21 21:38:28 -0400

    :wrench: webpack alias

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -15,6 +15,12 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
     alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@containers": path.resolve(__dirname, "src/containers"),
+      "@icons": path.resolve(__dirname, "src/assets/icons"),
+      "@logos": path.resolve(__dirname, "src/assets/logos"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@styles": path.resolve(__dirname, "src/styles"),
     },
   },
   module: {
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
     alias: {
+
+      "@components": path.resolve(__dirname, "src/components"),
+      "@containers": path.resolve(__dirname, "src/containers"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@styles": path.resolve(__dirname, "src/styles"),
     },
   },
   module: {
