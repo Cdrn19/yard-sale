83a422f5f43ed09f5082a03cbd6ce810e0ae825a
Carlos Revetti <cdrn_19@hotmail.com>
2023-03-26 14:50:40 -0400

    :sparkles: home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import ProductList from "@containers/ProductList";
+
+const Home = () => {
+  return (
+    <>
+      <ProductList />
+    </>
+  );
+};
+
+export default Home;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ProviderCart } from "@hooks/useShoppingCart";
 import { ProviderAuth } from "@hooks/useAuth";
 import Layout from "@containers/Layout";
+import Home from "@pages/Home";
 import Login from "@pages/Login";
 import "@styles/global.scss";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <ProviderCart>
       <ProviderAuth>
-        <Layout>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </Layout>
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
       </ProviderAuth>
-    </BrowserRouter>
+    </ProviderCart>
   );
 };
 
