0feb475283efd706e8255cff6a281a1b64bf2744
Carlos Revetti <cdrn_19@hotmail.com>
2023-03-27 22:38:52 -0400

    :rocket: implemented search input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,51 @@
-import ProductList from "@containers/ProductList";
+import Search from "@containers/Search";
 
 const Home = () => {
+  const products = [
+    {
+      id: 1,
+      title: "bolso",
+      price: 109.95,
+      description:
+        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+      category: "men's clothing",
+      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      rating: { rate: 3.9, count: 120 },
+    },
+    {
+      id: 2,
+      title: "bolso y msa",
+      price: 109.95,
+      description:
+        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+      category: "men's clothing",
+      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      rating: { rate: 3.9, count: 120 },
+    },
+    {
+      id: 3,
+      title: "lampara",
+      price: 109.95,
+      description:
+        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+      category: "men's clothing",
+      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      rating: { rate: 3.9, count: 120 },
+    },
+    {
+      id: 4,
+      title: "pedra",
+      price: 109.95,
+      description:
+        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+      category: "men's clothing",
+      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      rating: { rate: 3.9, count: 120 },
+    },
+  ];
   return (
     <>
-      <ProductList />
+      <Search products={products} />
     </>
   );
 };
