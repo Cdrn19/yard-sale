54e4d6deec7bf5d66bbaae9bc41774a47beee9d9
Carlos Revetti <cdrn_19@hotmail.com>
2023-03-27 22:36:45 -0400

    :sparkles: search input

---
+++ src/containers/Search.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import ProductList from "@containers/ProductList";
+import "@styles/Search.scss";
+
+import searchIcon from "@icons/search.svg";
+import arrow from "@icons/arrow.svg";
+
+const Search = ({ products }) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    const value = event.target.value;
+    setValue(value);
+  };
+
+  const onSearch = () => {
+    return products.filter((product) => {
+      return (
+        product.title
+          ?.toString()
+          .toLowerCase()
+          .indexOf(value.toString().toLowerCase().trim()) > -1
+      );
+    });
+  };
+
+  return (
+    <>
+      <div className="search">
+        <div className="search__container">
+          <div className="search__input">
+            <img src={searchIcon} alt="search" />
+            <input
+              placeholder="Search product"
+              type="text"
+              value={value}
+              onChange={onChange}
+            />
+          </div>
+        </div>
+        <div className="search__dropdown">{}</div>
+        <div className="search__container--filter">
+          <h5>Order:</h5>
+          <div className="search__button--filter">
+            <button>Most recent</button>
+            <img src={arrow} alt="arrow" />
+          </div>
+        </div>
+      </div>
+      <ProductList products={onSearch(value)} />
+    </>
+  );
+};
+
+Search.propTypes = {
+  products: PropTypes.array,
+};
+
+export default Search;
