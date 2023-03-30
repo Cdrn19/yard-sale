244e4b32b7387dd8f12d3b58566570c7d4f175d4
Carlos Revetti <cdrn_19@hotmail.com>
2023-03-29 20:00:19 -0400

    :rocket: categories service

--- src/service/index.js
+++ src/service/index.js
@@ -9,6 +9,13 @@ const endPoints = {
   products: {
     all: `${API}/${VERSION}/products`,
   },
+  categories: {
+    clothes: `${API}/${VERSION}/categories/1/products`,
+    electronics: `${API}/${VERSION}/categories/2/products`,
+    furnitures: `${API}/${VERSION}/categories/3/products`,
+    toys: `${API}/${VERSION}/categories/4/products`,
+    others: `${API}/${VERSION}/categories/5/products`,
+  },
 };
 
 export default endPoints;
