fecf8620c18f4430035eda4d7d378ec0e4825730
Carlos Revetti <cdrn_19@hotmail.com>
2023-04-10 01:09:46 -0400

    :rocket: send email implemented

--- src/pages/PasswordRecovery.jsx
+++ src/pages/PasswordRecovery.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import EmailValidation from "@containers/EmailValidation";
+import SendEmail from "@containers/SendEmail";
 
 const PasswordRecovery = () => {
   const [email, setEmail] = useState(null);
 
   const validateSendEmail = () => {
-    return <EmailValidation setEmail={setEmail} />;
+    if (email) {
+      return <SendEmail email={email} />;
+    } else {
+      return <EmailValidation setEmail={setEmail} />;
+    }
   };
 
   return <>{validateSendEmail()}</>;
