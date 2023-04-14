850a6bf774ae07e792f227e8455d694775e29621
Carlos Revetti <cdrn_19@hotmail.com>
2023-04-14 01:47:28 -0400

    :sparkles: checkout page

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,45 @@
+import { useShoppingCart } from "@hooks/useShoppingCart";
+import OrderItem from "@components/OrderItem";
+import "@styles/Checkout.scss";
+
+const Checkout = () => {
+  const { state } = useShoppingCart();
+
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const currentDate = `${month}.${day}.${year}`;
+
+  const sumCart = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  };
+
+  return (
+    <div className="checkout">
+      <div className="checkout__container">
+        <h1 className="checkout__container--title">My order</h1>
+        <div className="checkout__content">
+          <div className="checkout__content--order">
+            <p>
+              <span>{currentDate}</span>
+              <span>
+                {state.cart.length}{" "}
+                {state.cart.length <= 1 ? "article" : "articles"}
+              </span>
+            </p>
+            <p>{sumCart()} USD</p>
+          </div>
+        </div>
+        {state.cart.map((product) => (
+          <OrderItem key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
