c026578f3fe3499e043d682d7e58cc40dbb52a07
Carlos Revetti <cdrn_19@hotmail.com>
2023-04-17 20:04:55 -0400

    :bug: implemented seo component and a fix

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import { ProviderCart } from "@hooks/useShoppingCart";
 import { ProviderAuth } from "@hooks/useAuth";
 import { AuthRoute } from "@hooks/useAuthRoute";
 import Layout from "@containers/Layout";
 import Home from "@pages/Home";
-import Login from "@containers/Login";
+import SignIn from "@pages/SignIn";
 import CreateAccount from "@pages/CreateAccount";
 import PasswordRecovery from "@pages/PasswordRecovery";
 import Account from "@pages/Account";
@@ -14,31 +15,36 @@ import "@styles/global.scss";
 
 const App = () => {
   return (
-    <ProviderCart>
-      <ProviderAuth>
-        <BrowserRouter>
-          <Layout>
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route path="/category/:category" element={<Home />} />
-              <Route path="/signin" element={<Login />} />
-              <Route path="/signup" element={<CreateAccount />} />
-              <Route path="/passwordRecovery" element={<PasswordRecovery />} />
-              <Route path="/checkout" element={<Checkout />} />
-              <Route
-                path="/account"
-                element={
-                  <AuthRoute>
-                    <Account />
-                  </AuthRoute>
-                }
-              />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Layout>
-        </BrowserRouter>
-      </ProviderAuth>
-    </ProviderCart>
+    <HelmetProvider>
+      <ProviderCart>
+        <ProviderAuth>
+          <BrowserRouter>
+            <Layout>
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/category/:category" element={<Home />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<CreateAccount />} />
+                <Route
+                  path="/passwordRecovery"
+                  element={<PasswordRecovery />}
+                />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route
+                  path="/account"
+                  element={
+                    <AuthRoute>
+                      <Account />
+                    </AuthRoute>
+                  }
+                />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Layout>
+          </BrowserRouter>
+        </ProviderAuth>
+      </ProviderCart>
+    </HelmetProvider>
   );
 };
 
